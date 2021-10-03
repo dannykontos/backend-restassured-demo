1fb08fb12baf4d3fce622724b0e0452f20c8f7eb
Daniel Kontos <daniel.kontos@softvision.com>
2021-10-04 01:28:38 +0300

    Setup and first test structure

---
+++ backend-restassured-demo-tests/src/test/java/org/example/Payload.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Payload {
+
+    public String AddEmployee() {
+        return "{\\\"name\\\":\\\"Jack Turner\\\",\\\"salary\\\":\\\"123\\\",\\\"age\\\":\\\"23\\\"}";
+    }
+}
